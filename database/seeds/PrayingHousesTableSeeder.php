fd834ba8effbdb17d35dc0ddcc41e079f19980eb
João Moita Manrique <joaomoitamanrique@gmail.com>
2019-06-10 20:21:20 -0300

    Cria semeador da tabela de casas de oração

---
+++ database/seeds/PrayingHousesTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Models\Cooperator;
+use App\Models\PrayingHouse;
+
+use Illuminate\Database\Seeder;
+
+class PrayingHousesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $cooperators = Cooperator::all();
+
+        foreach($cooperators as $cooperator) {
+            $prayingHouse = factory(PrayingHouse::class, 1)->create([
+                'cooperator_craft_id' => $cooperator->id,
+                'administrative_region_id' => $cooperator->administrative_region_id,
+            ]);
+        }
+    }
+}

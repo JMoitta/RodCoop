a1771648b33ba346b637275bc92bc907f93c785b
João Moita Manrique <joaomoitamanrique@gmail.com>
2019-06-10 20:18:51 -0300

    Cria tabela de casas de orações

---
+++ database/migrations/2019_06_08_113917_create_praying_houses_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePrayingHousesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('praying_houses', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('locality');
+            $table->boolean('saturday');
+            $table->time('saturdayHours')->nullable();
+            $table->boolean('sunday');
+            $table->time('sundayHours')->nullable();
+            $table->string('address');
+            $table->bigInteger('cooperator_craft_id')->unsigned();
+            $table->foreign('cooperator_craft_id')->references('id')->on('cooperators');
+            $table->bigInteger('administrative_region_id')->unsigned();
+            $table->foreign('administrative_region_id')->references('id')->on('administrative_regions');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('praying_houses');
+    }
+}

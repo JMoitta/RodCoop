5cade10101865ccfa72a2cc8b83f03cdc0104d45
João Moita Manrique <joaomoitamanrique@gmail.com>
2019-06-10 21:16:51 -0300

    Implementa metodo index de PrayingHouseController

---
+++ app/Http/Controllers/Admin/PrayingHouseController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use \App\Models\PrayingHouse;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PrayingHouseController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $listPrayerOfHouses = PrayingHouse::paginate(5);
+        return view('admin.houses-of-prayer.index', \compact('listPrayerOfHouses'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\PrayingHouse  $prayingHouse
+     * @return \Illuminate\Http\Response
+     */
+    public function show(PrayingHouse $prayingHouse)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\PrayingHouse  $prayingHouse
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(PrayingHouse $prayingHouse)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\PrayingHouse  $prayingHouse
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, PrayingHouse $prayingHouse)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\PrayingHouse  $prayingHouse
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(PrayingHouse $prayingHouse)
+    {
+        //
+    }
+}

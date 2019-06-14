403dfd5ac5c4266a0fb80196f38e16984083723d
João Moita Manrique <joaomoitamanrique@gmail.com>
2019-06-13 23:13:48 -0300

    Cria tela de show de casas de oração

---
+++ resources/views/admin/houses-of-prayer/show.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts.app')
+
+@section('content')
+    <h3>{{ __('Show cooperator') }}</h3>
+    <div class="btn-toolbar mb-3">
+        <div class="btn-group mr-2">
+            <a href="{{ route('admin.praying-houses.edit', [$prayingHouse->id]) }}" class="btn btn-primary">Editar</a>
+        </div>
+        <div class="btn-group mr-2">
+            <button type="button" data-toggle="modal" data-target="#exampleModalCenter" class="btn btn-danger">
+            Excluir
+            </button>
+        </div>
+    </div>
+    <form id="formDelete1" action="{{ route('admin.praying-houses.destroy', [$prayingHouse->id]) }}" method="POST">
+        @csrf
+        @method('DELETE')
+    </form>
+    <table class="table table-bordered">
+        <tbody>
+            <tr>
+                <th scope="row">ID</th>
+                <td>{{ $prayingHouse->id }}</td>
+            </tr>
+            <tr>
+                <th scope="row">Localidade</th>
+                <td>{{ $prayingHouse->locality }}</td>
+            </tr>
+            <tr>
+                <th scope="row">Região Administrativa</th>
+                <td>{{ $prayingHouse->administrativeRegion->description }}</td>
+            </tr>
+            <tr>
+                <th scope="row">Cooperador</th>
+                <td>{{ $prayingHouse->cooperatorCraft->name }}</td>
+            </tr>
+            <tr>
+                <th scope="row">Endereço</th>
+                <td>{{ $prayingHouse->address }}</td>
+            </tr>
+        </tbody>
+    </table>
+@endsection

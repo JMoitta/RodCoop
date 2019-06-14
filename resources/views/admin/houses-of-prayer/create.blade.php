49f4a96190ee9fa15ba52cfb65597f20d23f980d
João Moita Manrique <joaomoitamanrique@gmail.com>
2019-06-13 23:24:32 -0300

    Corrige o nome da rota

--- resources/views/admin/houses-of-prayer/create.blade.php
+++ resources/views/admin/houses-of-prayer/create.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <h3>{{ __('New House of Prayer') }}</h3>
     @include('form._form_errors')
-    {{ Form::open(['route' => 'admin.houses-of-prayer.store']) }}
+    {{ Form::open(['route' => 'admin.praying-houses.store']) }}
         @include('admin.houses-of-prayer._form')
         <button type="submit" class="btn btn-primary">{{ __('Submit') }}</button>
     {{ Form::close() }}

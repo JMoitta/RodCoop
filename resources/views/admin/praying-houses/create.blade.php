69ea51d64f21c73d2eead0a13eca8c6b69ecd46b
João Moita Manrique <joaomoitamanrique@gmail.com>
2019-06-15 11:37:34 -0300

    Renomeia a pasta de casas de oração

--- resources/views/admin/praying-houses/create.blade.php
+++ resources/views/admin/praying-houses/create.blade.php
@@ -4,7 +4,7 @@
     <h3>{{ __('New House of Prayer') }}</h3>
     @include('form._form_errors')
     {{ Form::open(['route' => 'admin.praying-houses.store']) }}
-        @include('admin.houses-of-prayer._form')
+        @include('admin.praying-houses._form')
         <button type="submit" class="btn btn-primary">{{ __('Submit') }}</button>
     {{ Form::close() }}
 @endsection
--- resources/views/admin/praying-houses/edit.blade.php
+++ resources/views/admin/praying-houses/edit.blade.php
@@ -4,7 +4,7 @@
     <h3>{{ __('Edit Administrative Region') }}</h3>
     @include('form._form_errors')
     {{ Form::model($prayingHouse, ['route' => ['admin.praying-houses.update',$prayingHouse->id], 'method' => 'PUT']) }}
-        @include('admin.houses-of-prayer._form')
+        @include('admin.praying-houses._form')
         <button type="submit" class="btn btn-primary">{{ __('To Save') }}</button>
     {{ Form::close() }}
 @endsection

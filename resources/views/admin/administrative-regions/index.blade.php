28d821132dc9eed3efe453b402e2cbfab32a1b52
João Moita Manrique <joaomoitamanrique@gmail.com>
2019-06-21 21:56:51 -0300

    Inseri a listagem de região adm. dentro do card

--- resources/views/admin/administrative-regions/index.blade.php
+++ resources/views/admin/administrative-regions/index.blade.php
@@ -1,40 +1,49 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="row col mb-2">
-        <h3>{{ __('List of Regions Administration') }}</h3>
-        <a class="btn btn-outline-primary ml-2" href="{{ route('admin.administrative-regions.create') }}">{{ __('Create new') }}</a>
-    </div>
-    @if(count($listAdministrativeRegions))
-        <table class="table table-striped" id="table-search">
-            <thead>
-            <tr>
-                <th>ID</th>
-                <th>{{ __('Description') }}</th>
-                <th>Ações</th>
-            </tr>
-            </thead>
-            <tbody>
-            @foreach($listAdministrativeRegions as $administrativeRegion)
-                <tr>
-                    <td>{{ $administrativeRegion->id }}</td>
-                    <td>{{ $administrativeRegion->description }}</td>
-                    <td>
-                        <a href="{{ route('admin.administrative-regions.edit', $administrativeRegion->id)}}">{{ __('Edit') }}</a> |
-                        <a href="{{ route('admin.administrative-regions.show', $administrativeRegion->id)}}">{{ __('Show') }}</a>
-                    </td>
-                </tr>
-            @endforeach
-            </tbody>
-        </table>
-        <div class="float-right">
-            {!! $listAdministrativeRegions->appends(['search' => \Request::get('search'),'field_order' => \Request::get('field_order'),'order' =>\Request::get('order')])->links() !!}
+    <div class="card">
+        <div class="card-header">
+            <div class="row col">
+                <h3>{{ __('List of Regions Administration') }}</h3>
+                <a class="btn btn-outline-primary ml-2" href="{{ route('admin.administrative-regions.create') }}">{{ __('Create new') }}</a>
+            </div>
         </div>
-    @else
-    <table class="table">
-        <tr>
-            <td>Nenhum registrado encontrado</td>
-        </tr>
-    </table>
+        <div class="card-body">
+                @if(count($listAdministrativeRegions))
+                <div class="float-right">
+                    {!! $listAdministrativeRegions->appends(['search' => \Request::get('search'),'field_order' => \Request::get('field_order'),'order' =>\Request::get('order')])->links() !!}
+                </div>
+                <table class="table table-striped" id="table-search">
+                    <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>{{ __('Description') }}</th>
+                        <th>Ações</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    @foreach($listAdministrativeRegions as $administrativeRegion)
+                        <tr>
+                            <td>{{ $administrativeRegion->id }}</td>
+                            <td>{{ $administrativeRegion->description }}</td>
+                            <td>
+                                <a href="{{ route('admin.administrative-regions.edit', $administrativeRegion->id)}}">{{ __('Edit') }}</a> |
+                                <a href="{{ route('admin.administrative-regions.show', $administrativeRegion->id)}}">{{ __('Show') }}</a>
+                            </td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+                <div class="float-right">
+                    {!! $listAdministrativeRegions->appends(['search' => \Request::get('search'),'field_order' => \Request::get('field_order'),'order' =>\Request::get('order')])->links() !!}
+                </div>
+            @else
+            <table class="table">
+                <tr>
+                    <td>Nenhum registrado encontrado</td>
+                </tr>
+            </table>
+        </div>
+    </div>
 @endif
 @endsection

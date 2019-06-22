58cd114ab74601b012d36760c8cb7b4e5f84dcb3
João Moita Manrique <joaomoitamanrique@gmail.com>
2019-06-21 22:36:56 -0300

    Adiciona card em mostrar região administrativa

--- resources/views/admin/administrative-regions/show.blade.php
+++ resources/views/admin/administrative-regions/show.blade.php
@@ -1,36 +1,52 @@
 @extends('layouts.app')
 
 @section('content')
-    <h3>{{ __('Show administrative region') }}</h3>
-    <div class="btn-toolbar mb-3">
-        <div class="btn-group mr-2">
-            <a href="{{ route('admin.administrative-regions.edit', [$administrativeRegion->id]) }}" class="btn btn-primary">Editar</a>
+    <div class="card">
+        <div class="card-header">
+            <h3>{{ __('Show administrative region') }}</h3>
         </div>
-        <div class="btn-group mr-2">
-            @include('_show.delete_action',[
-                'model' => $administrativeRegion,
-                'action' => [
-                    'class' => 'btn-danger',
-                    'route' => 'admin.administrative-regions.destroy',
-                    'parameters' => $administrativeRegion->id,
-                    'label' => __('Delete'),
-                ],
-                'index' => $administrativeRegion->id,
-            ])
-        </div>
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item">
+                <table class="table table-sm table-borderless">
+                    <tbody>
+                        <tr>
+                            <th  scope="row" style="width:12%;text-align: right;">ID</th>
+                            <td>{{ $administrativeRegion->id }}</td>
+                        </tr>
+                        <tr>
+                            <th scope="row" style="text-align: right;">{{ __('Description') }}</th>
+                            <td>{{ $administrativeRegion->description }}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </li>
+            <li class="list-group-item">
+                <div class="btn-toolbar aling-right float-right">
+                    <div class="btn-group ml-2">
+                        <a href="#" class="btn btn-secondary">Lista de Rodízios</a>
+                    </div>
+                    <div class="btn-group ml-2">
+                        <a href="{{ route('admin.administrative-regions.edit', [$administrativeRegion->id]) }}" class="btn btn-primary">Editar</a>
+                    </div>
+                    <div class="btn-group ml-2">
+                        @include('_show.delete_action',[
+                            'model' => $administrativeRegion,
+                            'action' => [
+                                'class' => 'btn-danger',
+                                'route' => 'admin.administrative-regions.destroy',
+                                'parameters' => $administrativeRegion->id,
+                                'label' => __('Delete'),
+                            ],
+                            'index' => $administrativeRegion->id,
+                        ])
+                    </div>
+                </div>
+            </li>
+        </ul>
     </div>
+    
+    
     @section('formAction')
     @show
-    <table class="table table-bordered">
-        <tbody>
-            <tr>
-                <th scope="row">ID</th>
-                <td>{{ $administrativeRegion->id }}</td>
-            </tr>
-            <tr>
-                <th scope="row">{{ __('Descrition') }}</th>
-                <td>{{ $administrativeRegion->description }}</td>
-            </tr>
-        </tbody>
-    </table>
+    
 @endsection

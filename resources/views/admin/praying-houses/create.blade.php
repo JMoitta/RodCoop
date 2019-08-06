dba3e549e841ba0b09916aa1fb8b88da495e4339
João Moita Manrique <joaomoitamanrique@gmail.com>
2019-08-05 22:58:31 -0300

    Adiciona painel na pagina de criar

--- resources/views/admin/praying-houses/create.blade.php
+++ resources/views/admin/praying-houses/create.blade.php
@@ -1,10 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
-    <h3>{{ __('New House of Prayer') }}</h3>
-    @include('form._form_errors')
-    {{ Form::open(['route' => 'admin.praying-houses.store']) }}
-        @include('admin.praying-houses._form')
-        <button type="submit" class="btn btn-primary">{{ __('Submit') }}</button>
-    {{ Form::close() }}
+    <div class="card">
+        <div class="card-header">
+            {{ __('New House of Prayer') }}
+        </div>
+        <div class="card-body">
+            @include('form._form_errors')
+            {{ Form::open(['route' => 'admin.praying-houses.store']) }}
+                @include('admin.praying-houses._form')
+                <div class="btn-group" role="group">
+                    <button type="button" class="btn btn-secondary" onclick="comeBack()">{{__('Come back')}}</button>
+                    <button type="submit" class="btn btn-primary">{{ __('Submit') }}</button>
+                </div>
+            {{ Form::close() }}
+        </div>
+    </div>
+    <script type="text/javascript">
+        function comeBack() {
+            window.location = "{{ route('admin.praying-houses.index') }}"
+        }
+    </script>
 @endsection

fcad9f85293a4d8cd1062cb082bc1594e17994a1
João Moita Manrique <joaomoitamanrique@gmail.com>
2019-08-05 22:59:45 -0300

    Adiciona paine na pagina de mostrar

--- resources/views/admin/praying-houses/show.blade.php
+++ resources/views/admin/praying-houses/show.blade.php
@@ -1,48 +1,53 @@
 @extends('layouts.app')
 
 @section('content')
-    <h3>{{ __('Show cooperator') }}</h3>
-    <div class="btn-toolbar mb-3">
-        <div class="btn-group mr-2">
-            <a href="{{ route('admin.praying-houses.edit', [$prayingHouse->id]) }}" class="btn btn-primary">Editar</a>
+    <div class="card">
+        <div class="card-header">
+            {{ __('Show cooperator') }}
         </div>
-        <div class="btn-group mr-2">
-            @include('_show.delete_action',[
-                'model' => $prayingHouse,
-                'action' => [
-                    'class' => 'btn-danger',
-                    'route' => 'admin.praying-houses.destroy',
-                    'parameters' => $prayingHouse->id,
-                    'label' => __('Delete'),
-                ],
-                'index' => $prayingHouse->id,
-            ])
+        <div class="card-body">
+            <table class="table table-sm table-borderless">
+                <tbody>
+                    <tr>
+                        <th scope="row">ID</th>
+                        <td>{{ $prayingHouse->id }}</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Localidade</th>
+                        <td>{{ $prayingHouse->locality }}</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Região Administrativa</th>
+                        <td>{{ $prayingHouse->administrativeRegion->description }}</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Cooperador</th>
+                        <td>{{ $prayingHouse->cooperatorCraft->name }}</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Endereço</th>
+                        <td>{{ $prayingHouse->address }}</td>
+                    </tr>
+                </tbody>
+            </table>
+            <div class="btn-group ml-2 aling-right float-right">
+                <a href="{{ route('admin.praying-houses.edit', [$prayingHouse->id]) }}" class="btn btn-primary">Editar</a>
+                @include('_show.delete_action',[
+                    'model' => $prayingHouse,
+                    'action' => [
+                        'class' => 'btn-danger',
+                        'route' => 'admin.praying-houses.destroy',
+                        'parameters' => $prayingHouse->id,
+                        'label' => __('Delete'),
+                    ],
+                    'message' => __('Are you sure you want to delete a prayer house?'),
+                    'index' => $prayingHouse->id,
+                    ])
+            </div>
         </div>
     </div>
     @section('formAction')
     @show
-    <table class="table table-bordered">
-        <tbody>
-            <tr>
-                <th scope="row">ID</th>
-                <td>{{ $prayingHouse->id }}</td>
-            </tr>
-            <tr>
-                <th scope="row">Localidade</th>
-                <td>{{ $prayingHouse->locality }}</td>
-            </tr>
-            <tr>
-                <th scope="row">Região Administrativa</th>
-                <td>{{ $prayingHouse->administrativeRegion->description }}</td>
-            </tr>
-            <tr>
-                <th scope="row">Cooperador</th>
-                <td>{{ $prayingHouse->cooperatorCraft->name }}</td>
-            </tr>
-            <tr>
-                <th scope="row">Endereço</th>
-                <td>{{ $prayingHouse->address }}</td>
-            </tr>
-        </tbody>
-    </table>
+    @section('modal')
+    @show
 @endsection

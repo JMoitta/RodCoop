33cb2460ed30d4ebcce4f82e30e370fb45e19cd0
João Moita Manrique <joaomoitamanrique@gmail.com>
2019-08-05 22:59:18 -0300

    Adiciona painel na pagina de editar

--- resources/views/admin/praying-houses/edit.blade.php
+++ resources/views/admin/praying-houses/edit.blade.php
@@ -1,10 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
-    <h3>{{ __('Edit Administrative Region') }}</h3>
-    @include('form._form_errors')
-    {{ Form::model($prayingHouse, ['route' => ['admin.praying-houses.update',$prayingHouse->id], 'method' => 'PUT']) }}
-        @include('admin.praying-houses._form')
-        <button type="submit" class="btn btn-primary">{{ __('To Save') }}</button>
-    {{ Form::close() }}
+    <div class="card">
+        <div class="card-header">
+            {{ __('Edit Administrative Region') }}
+        </div>
+        <div class="card-body">
+            @include('form._form_errors')
+            {{ Form::model($prayingHouse, ['route' => ['admin.praying-houses.update',$prayingHouse->id], 'method' => 'PUT']) }}
+                @include('admin.praying-houses._form')
+                <div class="btn-group" role="group">
+                    <button type="button" class="btn btn-secondary" onclick="comeBack()">{{__('Come back')}}</button>
+                    <button type="submit" class="btn btn-primary">{{ __('To Save') }}</button>
+                </div>
+            {{ Form::close() }}
+        </div>
+    </div>
+    <script type="text/javascript">
+        function comeBack() {
+            window.location = "{{ route('admin.praying-houses.index') }}"
+        }
+    </script>
 @endsection

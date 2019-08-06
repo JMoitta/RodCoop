0315f2b0f2dd95a1af9cf67363b046cfe2d6f96d
João Moita Manrique <joaomoitamanrique@gmail.com>
2019-08-05 22:34:06 -0300

    Adiciona painel na pagina de edição

--- resources/views/admin/administrative-regions/edit.blade.php
+++ resources/views/admin/administrative-regions/edit.blade.php
@@ -1,10 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
-    <h3>{{ __('Edit Administrative Region') }}</h3>
-    @include('form._form_errors')
-    {{ Form::model($administrativeRegion, ['route' => ['admin.administrative-regions.update',$administrativeRegion->id], 'method' => 'PUT']) }}
-        @include('admin.administrative-regions._form')
-        <button type="submit" class="btn btn-primary">{{ __('To Save') }}</button>
-    {{ Form::close() }}
+    <div class="card">
+        <div class="card-header">
+            {{ __('Edit Administrative Region') }}
+        </div>
+        <div class="card-body">
+            @include('form._form_errors')
+            {{ Form::model($administrativeRegion, ['route' => ['admin.administrative-regions.update',$administrativeRegion->id], 'method' => 'PUT']) }}
+                @include('admin.administrative-regions._form')
+                <div class="btn-group" role="group">
+                    <button type="button" class="btn btn-secondary" onclick="comeBack()">{{__('Come back')}}</button>
+                    <button type="submit" class="btn btn-primary">{{ __('To Save') }}</button>
+                </div>
+            {{ Form::close() }}
+        </div>
+    </div>
+    <script type="text/javascript">
+        function comeBack() {
+            window.location = "{{ route('admin.administrative-regions.index') }}"
+        }
+    </script>
 @endsection

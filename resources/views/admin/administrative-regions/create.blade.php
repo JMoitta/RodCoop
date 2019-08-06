412d4c9d2639def9d6e39e2b3e93d55e58fdd9d3
João Moita Manrique <joaomoitamanrique@gmail.com>
2019-08-05 22:33:35 -0300

    Adiciona painel na pagina de criar

--- resources/views/admin/administrative-regions/create.blade.php
+++ resources/views/admin/administrative-regions/create.blade.php
@@ -1,10 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
-    <h3>{{ __('New Administrative Region') }}</h3>
-    @include('form._form_errors')
-    {{ Form::open(['route' => 'admin.administrative-regions.store']) }}
-        @include('admin.administrative-regions._form')
-        <button type="submit" class="btn btn-primary">{{ __('Submit') }}</button>
-    {{ Form::close() }}
+    <div class="card">
+        <div class="card-header">
+            {{ __('New Administrative Region') }}
+        </div>
+        <div class="card-body">
+            @include('form._form_errors')
+            {{ Form::open(['route' => 'admin.administrative-regions.store']) }}
+                @include('admin.administrative-regions._form')
+                <div class="btn-group" role="group">
+                    <button type="button" class="btn btn-secondary" onclick="comeBack()">{{__('Come back')}}</button>
+                    <button type="submit" class="btn btn-primary">{{ __('Submit') }}</button>
+                </div>
+            {{ Form::close() }}
+        </div>
+    </div>
+    <script type="text/javascript">
+        function comeBack() {
+            window.location = "{{ route('admin.administrative-regions.index') }}"
+        }
+    </script>
 @endsection

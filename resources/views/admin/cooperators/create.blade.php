5a7c575c579eb3072cba41d9ef1eabed6c89c0d5
João Moita Manrique <joaomoitamanrique@gmail.com>
2019-08-06 22:11:53 -0300

    Adiciona painel na pagina de criar e editar

--- resources/views/admin/cooperators/create.blade.php
+++ resources/views/admin/cooperators/create.blade.php
@@ -1,10 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
-    <h3>{{ __('New Cooperator') }}</h3>
-    @include('form._form_errors')
-    {{ Form::open(['route' => 'admin.cooperators.store']) }}
-        @include('admin.cooperators._form')
-        <button type="submit" class="btn btn-primary">{{ __('Submit') }}</button>
-    {{ Form::close() }}
+    <div class="card">
+        <div class="card-header">
+            {{ __('New Cooperator') }}
+        </div>
+        <div class="card-body">
+            @include('form._form_errors')
+            {{ Form::open(['route' => 'admin.cooperators.store']) }}
+                @include('admin.cooperators._form')
+                <div class="btn-group" role="group">
+                    <button type="button" class="btn btn-secondary" onclick="comeBack()">{{__('Come back')}}</button>
+                    <button type="submit" class="btn btn-primary">{{ __('Submit') }}</button>
+                </div>
+            {{ Form::close() }}
+        </div>
+    </div>
+    <script type="text/javascript">
+        function comeBack() {
+            window.location = "{{ route('admin.cooperators.index') }}"
+        }
+    </script>
 @endsection
--- resources/views/admin/cooperators/edit.blade.php
+++ resources/views/admin/cooperators/edit.blade.php
@@ -1,10 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
-    <h3>{{ __('Edit Cooperator') }}</h3>
-    @include('form._form_errors')
-    {{ Form::model($cooperator, ['route' => ['admin.cooperators.update',$cooperator->id], 'method' => 'PUT']) }}
-        @include('admin.cooperators._form')
-        <button type="submit" class="btn btn-primary">{{ __('To Save') }}</button>
-    {{ Form::close() }}
+    <div class="card">
+        <div class="card-header">
+            {{ __('Edit Cooperator') }}
+        </div>
+        <div class="card-body">
+            @include('form._form_errors')
+            {{ Form::model($cooperator, ['route' => ['admin.cooperators.update',$cooperator->id], 'method' => 'PUT']) }}
+                @include('admin.cooperators._form')
+                <div class="btn-group" role="group">
+                    <button type="button" class="btn btn-secondary" onclick="comeBack()">{{__('Come back')}}</button>
+                    <button type="submit" class="btn btn-primary">{{ __('To Save') }}</button>
+                </div>
+            {{ Form::close() }}
+        </div>
+    </div>
+    <script type="text/javascript">
+        function comeBack() {
+            window.location = "{{ route('admin.cooperators.index') }}"
+        }
+    </script>
 @endsection

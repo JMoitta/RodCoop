422b8ee7f6c24f635736dc10c0835fcf452ddb53
João Moita Manrique <joaomoitamanrique@gmail.com>
2019-08-08 18:51:26 -0300

    Adiciona link para Dashboard

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,6 +26,7 @@
             <div class="collapse navbar-collapse" id="navbarNavHeader">
                 <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
                     @can('admin')
+                        <li class="nav-item"><a class="nav-link" href="{{ route('admin.') }}">{{ __('Dashboard') }}</a></li>
                         @can('root')
                         <li class="nav-item"><a class="nav-link" href="{{ route('admin.administrative-regions.index') }}">{{ __('Administrative regions') }}</a></li>
                         @endcan

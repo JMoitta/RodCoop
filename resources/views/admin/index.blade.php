c2c4d16f7fae2fb34d1f285d8702cbd7dc579f93
João Moita Manrique <joaomoitamanrique@gmail.com>
2019-08-08 23:10:52 -0300

    Altera pagina principal do admin

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,22 +1,49 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card border-secondary">
-                <div class="card-header text-white bg-secondary">Paniel Administrativo</div>
-
-                <div class="card-body text-secondary">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    Você está logado!
-                </div>
+    <div class="card">
+        <div class="card-header"><span class="font-weight-bold">{{__('Administrative region')}}:</span> {{Auth::user()->administrativeRegion->description}}</div>
+        <div class="card-body">
+            <div class="btn-group" role="group">
+                <button type="submit" class="btn btn-primary">{{ __('New caster list') }}</button>
             </div>
+        </div>
+    </div>
+    <div class="card mt-3">
+        <div class="card-header">
+            {{ __('New caster list') }}
+        </div>
+        <div class="card-body">
+            <table class="table">
+                <thead>
+                  <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Primeiro</th>
+                    <th scope="col">Último</th>
+                    <th scope="col">Nickname</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <th scope="row">1</th>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">2</th>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                    <td>@fat</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">3</th>
+                    <td>Larry</td>
+                    <td>the Bird</td>
+                    <td>@twitter</td>
+                  </tr>
+                </tbody>
+              </table>
         </div>
     </div>
 </div>

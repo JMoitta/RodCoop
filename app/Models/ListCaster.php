faa0821006efa555be4cfab84d76d80df558a0bc
João Moita Manrique <joaomoitamanrique@gmail.com>
2019-08-13 23:10:25 -0300

    Adiciona softDeletes

--- app/Models/ListCaster.php
+++ app/Models/ListCaster.php
@@ -6,9 +6,11 @@ use App\User;
 use App\Tenant\TenantModels;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ListCaster extends Model
 {
+    use SoftDeletes;
     use TenantModels;
     /**
      * The attributes that are mass assignable.

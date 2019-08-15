c7d9182045fd7179ad663a254bd889da4341f9dd
João Moita Manrique <joaomoitamanrique@gmail.com>
2019-08-15 07:08:16 -0300

    Remove required de regiao adiministrativa

--- app/Http/Requests/Cooperator/StoreCooperator.php
+++ app/Http/Requests/Cooperator/StoreCooperator.php
@@ -25,7 +25,6 @@ class StoreCooperator extends FormRequest
     {
         return [
             'name' => 'required|max:255',
-            'administrative_region_id' => 'required|exists:administrative_regions,id'
         ];
     }
 }

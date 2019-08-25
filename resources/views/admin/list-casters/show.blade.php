b1b5345a6c52b0fccac79c2ffc242d592c7832c7
João Moita Manrique <joaomoitamanrique@gmail.com>
2019-08-25 18:05:02 -0300

    Implementa blade de show

--- resources/views/admin/list-casters/show.blade.php
+++ resources/views/admin/list-casters/show.blade.php
@@ -6,32 +6,76 @@
             {{ __('Show administrative region') }}
         </div>
         <div class="card-body">
-            <table class="table table-sm table-borderless">
-                <tbody>
-                    <tr>
-                        <th  scope="row" style="width:12%;text-align: right;">ID</th>
-                        <td>{{ $administrativeRegion->id }}</td>
-                    </tr>
-                    <tr>
-                        <th scope="row" style="text-align: right;">{{ __('Description') }}</th>
-                        <td>{{ $administrativeRegion->description }}</td>
-                    </tr>
-                </tbody>
-            </table>
-            <div class="btn-group ml-2 aling-right float-right">
-                <a href="{{ route('admin.list-casters.index')}}" class="btn btn-secondary">Lista de Rodízios</a>
-                <a href="{{ route('admin.administrative-regions.edit', [$administrativeRegion->id]) }}" class="btn btn-primary">Editar</a>
-                @include('_show.delete_action',[
-                    'model' => $administrativeRegion,
-                    'action' => [
-                        'class' => 'btn-danger',
-                        'route' => 'admin.administrative-regions.destroy',
-                        'parameters' => $administrativeRegion->id,
-                        'label' => __('Delete'),
-                    ],
-                    'message' => __('Are you sure you want to delete an administrative region?'),
-                    'index' => $administrativeRegion->id,
-                    ])
+            <div class="row">
+                <div class="col">
+                    <form>
+                        <div class="form-row">
+                            <div class="col">
+                                {{ Form::select('cooperator_id', $listNameCooperator, null, ['class' => 'custom-select']) }}
+                            </div>
+                            <div class="col">
+                                <a class="btn btn-primary" href="{{ route('admin.list-casters.index')}}">Rodízio do Cooperador</a>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+                <div class="col">
+                    <form>
+                        <div class="form-row">
+                            <div class="col">
+                                {{ Form::select('cooperator_id', $listLocalityPrayingHouse, null, ['class' => 'custom-select']) }}
+                            </div>
+                            <div class="col">
+                                <a class="btn btn-primary" href="{{ 'sdf' }}">Rodízio da Casa de oração</a>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="card mt-3">
+        <div class="card-header">
+            {{ __('Show administrative region') }}
+        </div>
+        <div class="card-body">
+            <div class="table-responsive">
+                <table class="table table-striped " id="table-search">
+                    <thead>
+                        <tr>
+                            <th>{{ __('Houses of prayer') }}</th>
+                            @foreach($casterListItemsGroup as $prayingHouse => $casterListItems)
+                                @foreach ($casterListItems as $casterListItem)
+                                    <th class="border-left">{{ __('Caster Cooperator') }}</th>
+                                    <th>{{ date('m/Y', strtotime($casterListItem->date_caster)) }}</th>
+                                @endforeach
+                                @break
+                            @endforeach
+                        </tr>
+                    </thead>
+                    <tbody>
+                    @foreach($casterListItemsGroup as $prayingHouse => $casterListItems)
+                        <tr>
+                            <td class="font-weight-bold">{{ $prayingHouse }}</td>
+                            @foreach ($casterListItems as $casterListItem)
+                                <td class="border-left">{{ $casterListItem->cooperator}}</td>
+                                <td>
+                                @php
+                                    $dayOfWeek = date('l', strtotime($casterListItem->date_caster));
+                                    if($dayOfWeek == 'Sunday') {
+                                        $dayOfWeek = 'Domingo';
+                                    }
+                                    if($dayOfWeek == 'Saturday') {
+                                        $dayOfWeek = 'Sabádo';
+                                    }
+                                    echo $dayOfWeek . date(' - d/m/Y - G:i ', strtotime($casterListItem->date_caster));
+                                @endphp
+                                </td>
+                            @endforeach
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>

103d025d262719f40c7a9d0305eb944adc15f8c7
João Moita Manrique <joaomoitamanrique@gmail.com>
2019-08-25 17:38:14 -0300

    remove chave administrative_region_id de Request

--- app/Http/Requests/Cooperator/UpdateCooperator.php
+++ app/Http/Requests/Cooperator/UpdateCooperator.php
@@ -25,7 +25,6 @@ class UpdateCooperator extends FormRequest
     {
         return [
             'name' => 'required|max:255',
-            'administrative_region_id' => 'required|exists:administrative_regions,id'
         ];
     }
 }

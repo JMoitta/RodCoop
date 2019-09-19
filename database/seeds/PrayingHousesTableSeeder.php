89e336b2392a5e6b4606fcfcfc4c7b8999349616
João Moita Manrique <joaomoitamanrique@gmail.com>
2019-09-18 23:18:24 -0300

    Adiciona regra de culto de rodizio aleatorio

--- database/seeds/PrayingHousesTableSeeder.php
+++ database/seeds/PrayingHousesTableSeeder.php
@@ -18,9 +18,28 @@ class PrayingHousesTableSeeder extends Seeder
         $cooperators = Cooperator::all();
 
         foreach($cooperators as $cooperator) {
+            if(rand(1, 2) == 1){
+                $saturday = 'on';
+            } else {
+                $saturday = 'off';
+            }
+            if(rand(1, 2) == 1){
+                $sunday = 'on';
+            } else {
+                $sunday = 'off';
+            }
+            if($sunday == 'off' && $saturday == 'off'){
+                if(rand(1, 2) == 1){
+                    $sunday = 'on';
+                } else {
+                    $saturday = 'on';
+                }
+            }
             $prayingHouse = factory(PrayingHouse::class, 1)->create([
                 'cooperator_craft_id' => $cooperator->id,
                 'administrative_region_id' => $cooperator->administrative_region_id,
+                'saturday' => $saturday,
+                'sunday' => $sunday
             ]);
         }
     }
